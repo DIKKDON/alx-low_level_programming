d011f14207c1fe7d9ff87f873abe412fbb738246
DIKKDON <dicksonchideraudeh@gmail.com>
2022-09-08 17:36:40 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,7 @@
  *  * File: 5-printf.c
  *
  *   */
-
-
-
 #include <stdio.h>
-
-
 
 /**
  *
@@ -25,12 +20,8 @@
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
 
