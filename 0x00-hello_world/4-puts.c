b6a5379d2a758a9919984a2734b4450db8ad29af
DIKKDON <dicksonchideraudeh@gmail.com>
2022-09-09 14:28:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,4 @@
-/*
-
-     * File: 4-puts.c
-     *
-     *  */
-
-
-
 #include <stdio.h>
-
-
 
 /**
  *
@@ -27,9 +17,7 @@ int main(void)
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
+		
+		return (0);
 
 }
