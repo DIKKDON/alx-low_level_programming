fbb5c488e12d40dcfec4bddc21ad9eadeae4d2c5
DIKKDON <dicksonchideraudeh@gmail.com>
2022-09-09 15:46:34 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints "the size of various types based on the computer it is compiled and run on..
+ * main - Prints "the size of various types based on the computer it is compiled and run on,
  *
  * Return: Always 0.
  */
