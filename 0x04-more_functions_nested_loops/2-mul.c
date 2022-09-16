3a417a78754f24886f9b799f825e1ce27d3101f9
DIKKDON <dicksonchideraudeh@gmail.com>
2022-09-16 01:59:19 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two digits
+ * @a: 1st digit in the multiplication
+ * @b: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
