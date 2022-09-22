1aa0594c0fa3a218324dd23133dff2e6ac558916
DIKKDON <dicksonchideraudeh@gmail.com>
2022-09-22 05:02:30 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,20 @@
 
-
+/**
+ * _strcat - multiplies two digits
+ * @src: 1st digit in the multiplication
+ * @dest: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = j = 0;
+	i = 0;
+	j = 0;
 	while (dest[i] != '\0') /* find end of dest */
 		i++;
 	while ((dest[i++] = src[j++]) != '\0') /* copy src */
 		;
+	return (dest);
 }
