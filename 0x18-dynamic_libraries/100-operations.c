1fad6a67a6ba7a078c48fdf2a41f8dc4c36c2620
DIKKDON <dicksonchideraudeh@gmail.com>
2022-12-15 14:01:27 -0800

    advanced task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - description goes here
+ * @a: description goes here
+ * @b: description goes here
+ * Return: return value meaning goes here
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - description goes here
+ * @a: description goes here
+ * @b: description goes here
+ * Return: return value meaning goes here
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - description goes here
+ * @a: description goes here
+ * @b: description goes here
+ * Return: return value meaning goes here
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - description goes here
+ * @a: description goes here
+ * @b: description goes here
+ * Return: return value meaning goes here
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - description goes here
+ * @a: description goes here
+ * @b: description goes here
+ * Return: return value meaning goes here
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
